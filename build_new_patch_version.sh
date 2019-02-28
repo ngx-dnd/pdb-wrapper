74883ca146fa718e26c729835b1618c85f79857d
mirek001 <mirek.grzegorczyk@gmail.com>
2019-02-28 01:32:27 +0000

    build_new_patch fix

--- build_new_patch_version.sh
+++ build_new_patch_version.sh
@@ -6,7 +6,8 @@ npm version patch
 git tag
 git push origin --tags
 read -p "Build new version and push to npm? [Y/n]: " buildOption
-if [[ $buildOption = n ]]; then echo "OK, done"
+if [[ $buildOption ]]; then 
+        echo "OK, done"
 else
         ng build @ngx-dnd/pdb
         yarn publish /app/dist/ngx-dnd/pdb/
